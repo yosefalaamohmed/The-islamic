1748305eb951b16d266bf6891bef7d4b44c0ff4c
yosefalaamohmed <gothicplayer0@gmail.com>
2024-09-18 22:47:14 +0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+function showVerse() {
+    // هذا مثال بسيط لعرض نص عند الضغط على الزر
+    const verseElement = document.getElementById('verse');
+    verseElement.textContent = 'بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ'; // نص الآية كمثال
+}
